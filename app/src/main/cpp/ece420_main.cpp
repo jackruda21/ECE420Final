77a886c3889521db1c52084a9881c65b5121965d
jackruda21 <jcruda2@illinois.edu>
2023-04-27 20:15:59 -0500

    Removed specGate

--- app/src/main/cpp/ece420_main.cpp
+++ app/src/main/cpp/ece420_main.cpp
@@ -47,176 +47,176 @@ int first_frame = 10;
 float mean = 0;
 float stDev = 0;
 
-void ece420ProcessFrame(sample_buf *dataBuf) {
-    isWritingFft = false;
-    // Keep in mind, we only have 20ms to process each buffer!
-    struct timeval start;
-    struct timeval end;
-    gettimeofday(&start, NULL);
+// void ece420ProcessFrame(sample_buf *dataBuf) {
+//     isWritingFft = false;
+//     // Keep in mind, we only have 20ms to process each buffer!
+//     struct timeval start;
+//     struct timeval end;
+//     gettimeofday(&start, NULL);
 
-    // Data is encoded in signed PCM-16, little-endian, mono channel
-    float bufferIn[FRAME_SIZE];
-    for (int i = 0; i < FRAME_SIZE; i++) {
-        int16_t val = ((uint16_t) dataBuf->buf_[2 * i]) | (((uint16_t) dataBuf->buf_[2 * i + 1]) << 8);
-        bufferIn[i] = (float) val;
-    }
+//     // Data is encoded in signed PCM-16, little-endian, mono channel
+//     float bufferIn[FRAME_SIZE];
+//     for (int i = 0; i < FRAME_SIZE; i++) {
+//         int16_t val = ((uint16_t) dataBuf->buf_[2 * i]) | (((uint16_t) dataBuf->buf_[2 * i + 1]) << 8);
+//         bufferIn[i] = (float) val;
+//     }
 
-    // Spectrogram is just a fancy word for short time fourier transform
-    // 1. Apply hamming window to the entire FRAME_SIZE
-    // 2. Zero padding to FFT_SIZE = FRAME_SIZE * ZP_FACTOR
-    // 3. Apply fft with KISS_FFT engine
-    // 4. Scale fftOut[] to between 0 and 1 with log() and linear scaling
-    // NOTE: This code block is a suggestion to get you started. You will have to
-    // add/change code outside this block to implement FFT buffer overlapping (extra credit part).
-    // Keep all of your code changes within java/MainActivity and cpp/ece420_*
-    // ********************* START YOUR CODE HERE *********************** //
+//     // Spectrogram is just a fancy word for short time fourier transform
+//     // 1. Apply hamming window to the entire FRAME_SIZE
+//     // 2. Zero padding to FFT_SIZE = FRAME_SIZE * ZP_FACTOR
+//     // 3. Apply fft with KISS_FFT engine
+//     // 4. Scale fftOut[] to between 0 and 1 with log() and linear scaling
+//     // NOTE: This code block is a suggestion to get you started. You will have to
+//     // add/change code outside this block to implement FFT buffer overlapping (extra credit part).
+//     // Keep all of your code changes within java/MainActivity and cpp/ece420_*
+//     // ********************* START YOUR CODE HERE *********************** //
 
-    //window the frame
+//     //window the frame
 
-    for(int i =0; i<FRAME_SIZE;i++){
-        bufferIn[i] = bufferIn[i] * .54 - .46*cos((2*M_PI*i)/(FRAME_SIZE-1));
-    }
-    //zero pad
-    float s_pad[FRAME_SIZE * ZP_FACTOR] = {};
-    float magnitude[FRAME_SIZE] = {};
+//     for(int i =0; i<FRAME_SIZE;i++){
+//         bufferIn[i] = bufferIn[i] * .54 - .46*cos((2*M_PI*i)/(FRAME_SIZE-1));
+//     }
+//     //zero pad
+//     float s_pad[FRAME_SIZE * ZP_FACTOR] = {};
+//     float magnitude[FRAME_SIZE] = {};
 
-    // Fill in the zero_pad with data from the microphone
-    for(int i=0; i<FRAME_SIZE; i++){
-        s_pad[i] = bufferIn[i];
-        magnitude[i] = 0;
-    }
+//     // Fill in the zero_pad with data from the microphone
+//     for(int i=0; i<FRAME_SIZE; i++){
+//         s_pad[i] = bufferIn[i];
+//         magnitude[i] = 0;
+//     }
 
-    // The following is to for creating a STFT
-    int nfft = FRAME_SIZE * ZP_FACTOR;
-    int is_inverse_fft = 0;
-    kiss_fft_cpx cx_in[FRAME_SIZE * ZP_FACTOR] = {};   //input to be FFT'd
-    kiss_fft_cpx cx_out[FRAME_SIZE * ZP_FACTOR] = {};  //FFT'd output
+//     // The following is to for creating a STFT
+//     int nfft = FRAME_SIZE * ZP_FACTOR;
+//     int is_inverse_fft = 0;
+//     kiss_fft_cpx cx_in[FRAME_SIZE * ZP_FACTOR] = {};   //input to be FFT'd
+//     kiss_fft_cpx cx_out[FRAME_SIZE * ZP_FACTOR] = {};  //FFT'd output
 
-    kiss_fft_cfg cfg = kiss_fft_alloc( nfft ,is_inverse_fft,0,0);
-
-
-    for(int i=0; i<nfft; i++){
-
-        // put kth sample in cx_in[k].r and cx_in[k].i
-        cx_in[i].r = s_pad[i];
-        cx_in[i].i = 0;
-        //kiss_fft( cfg , cx_in , cx_out );
-        // transformed. DC is in cx_out[0].r and cx_out[0].i
-
-    }
-    kiss_fft( cfg , cx_in , cx_out );
-    free(cfg);
-
-    float min = FLT_MAX;
-    float max = -FLT_MAX;
-    //make output dB scale and find min and max
-    for(int i=0; i<nfft/2; i++){
-        cleanOut[i] = 20*log10(sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i));
-        fftOut[i] = 20*log10(sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i));
-        if(fftOut[i]<min){
-            min = fftOut[i];
-        }
-        if(fftOut[i]>max){
-            max = fftOut[i];
-        }
-    }
-
-    //cx_out is our FFT'd signal
-    //calculate mean and standard deviation
-    if(first_frame){
-        float sum = 0.0;
-        for (int i = 0; i < nfft/2; i++) {
-            sum += fftOut[i];
-        }
-        mean = sum/(nfft/2);
-        for (int i = 0; i < nfft/2; i++) {
-            stDev += pow(fftOut[i] - mean, 2);
-        }
-        stDev = sqrt(stDev/(nfft/2));
-        if(first_frame > 0){
-            first_frame --;
-        }
-    }
-    LOGD("Mean: %f, Max: %f, Min: %f, stDev: %f", mean, max, min, stDev);
+//     kiss_fft_cfg cfg = kiss_fft_alloc( nfft ,is_inverse_fft,0,0);
 
 
-    // Smoothing filter ... ignore for now
+//     for(int i=0; i<nfft; i++){
 
-    //apply masking
-    float thresh = mean + SENSE * stDev;
-    for (int i = 0; i < nfft/2; i++) {
-        if(fftOut[i] < thresh){
-            fftOut[i] = 0; //(min < 0 ? 0 : min); //for visualization, just set it = to 0
-            cx_out[i].i = 0;
-            cx_out[i].r = min;
-        }
-        fftOut[i] = fftOut[i]/max;
-        cleanOut[i] = cleanOut[i]/max;
-    }
+//         // put kth sample in cx_in[k].r and cx_in[k].i
+//         cx_in[i].r = s_pad[i];
+//         cx_in[i].i = 0;
+//         //kiss_fft( cfg , cx_in , cx_out );
+//         // transformed. DC is in cx_out[0].r and cx_out[0].i
+
+//     }
+//     kiss_fft( cfg , cx_in , cx_out );
+//     free(cfg);
+
+//     float min = FLT_MAX;
+//     float max = -FLT_MAX;
+//     //make output dB scale and find min and max
+//     for(int i=0; i<nfft/2; i++){
+//         cleanOut[i] = 20*log10(sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i));
+//         fftOut[i] = 20*log10(sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i));
+//         if(fftOut[i]<min){
+//             min = fftOut[i];
+//         }
+//         if(fftOut[i]>max){
+//             max = fftOut[i];
+//         }
+//     }
+
+//     //cx_out is our FFT'd signal
+//     //calculate mean and standard deviation
+//     if(first_frame){
+//         float sum = 0.0;
+//         for (int i = 0; i < nfft/2; i++) {
+//             sum += fftOut[i];
+//         }
+//         mean = sum/(nfft/2);
+//         for (int i = 0; i < nfft/2; i++) {
+//             stDev += pow(fftOut[i] - mean, 2);
+//         }
+//         stDev = sqrt(stDev/(nfft/2));
+//         if(first_frame > 0){
+//             first_frame --;
+//         }
+//     }
+//     LOGD("Mean: %f, Max: %f, Min: %f, stDev: %f", mean, max, min, stDev);
 
 
-/*
-    float max = -FLT_MAX;
-    for(int i=0; i<nfft/2; i++){
-        //fftOut[i] = 20*log10(sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i));
-        magnitude[i] = abs(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i); // Compute magnitude power spectrum
-        mean += magnitude[i] / 2; // Computes noise floor estimate
-        if(magnitude[i]>maxVal){maxVal = magnitude[i];} // Peak detection
-    }
-    mean /= (nfft/2);
-    //float noise_max;
-    for (int i=0; i<nfft/2; i++){
-        //compute noise magnitude spectrum
-        fftOut[i] = sqrt(fmax(magnitude[i] - floor,0));
-        fftOut[i] = fmax(magnitude[i] - fftOut[i], 0);
-    }
-*/
-/*
-    for(int i=0; i<nfft/2; i++){
-        fftOut[i] = fftOut[i]/max;
-    }
+//     // Smoothing filter ... ignore for now
 
-*/
+//     //apply masking
+//     float thresh = mean + SENSE * stDev;
+//     for (int i = 0; i < nfft/2; i++) {
+//         if(fftOut[i] < thresh){
+//             fftOut[i] = 0; //(min < 0 ? 0 : min); //for visualization, just set it = to 0
+//             cx_out[i].i = 0;
+//             cx_out[i].r = min;
+//         }
+//         fftOut[i] = fftOut[i]/max;
+//         cleanOut[i] = cleanOut[i]/max;
+//     }
 
 
+// /*
+//     float max = -FLT_MAX;
+//     for(int i=0; i<nfft/2; i++){
+//         //fftOut[i] = 20*log10(sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i));
+//         magnitude[i] = abs(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i); // Compute magnitude power spectrum
+//         mean += magnitude[i] / 2; // Computes noise floor estimate
+//         if(magnitude[i]>maxVal){maxVal = magnitude[i];} // Peak detection
+//     }
+//     mean /= (nfft/2);
+//     //float noise_max;
+//     for (int i=0; i<nfft/2; i++){
+//         //compute noise magnitude spectrum
+//         fftOut[i] = sqrt(fmax(magnitude[i] - floor,0));
+//         fftOut[i] = fmax(magnitude[i] - fftOut[i], 0);
+//     }
+// */
+// /*
+//     for(int i=0; i<nfft/2; i++){
+//         fftOut[i] = fftOut[i]/max;
+//     }
 
-    //audio playback
+// */
 
-    //ifft
-    is_inverse_fft = 1;
-    cfg = kiss_fft_alloc( nfft ,is_inverse_fft,0,0);
-    kiss_fft( cfg , cx_out , cx_in );
-    free(cfg);
 
-    uint16_t outmag = 0;
-    //set output buffer
-    for(int i=0; i<FRAME_SIZE; i++){
-        outmag = (uint16_t)sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i);
-        dataBuf->buf_[2*i] = (uint8_t)outmag;
-        dataBuf->buf_[2*i+1] = (uint8_t)(outmag>>8);
-    }
 
-    // thread-safe
-    isWritingFft = true;
-    // Currently set everything to 0 or 1 so the spectrogram will just be blue and red stripped
-    /*for ( int i= 0; i < FRAME_SIZE; i++) {
-        fftOut[i] = (i/20)%2;
-    }*/
+//     //audio playback
 
-    /*
-    Given a buffer size N and a sampling rate Fs, how much time do you have to complete your
-    processing before the next buffer comes in?
+//     //ifft
+//     is_inverse_fft = 1;
+//     cfg = kiss_fft_alloc( nfft ,is_inverse_fft,0,0);
+//     kiss_fft( cfg , cx_out , cx_in );
+//     free(cfg);
 
-    N * 1/Fs because we are getting Fs samples/sec, so each sample takes 1/Fs time to get.
-    Since there are N samples per buffer, the buffer will fill up in N * 1/Fs seconds.
-    */
-    // ********************* END YOUR CODE HERE ************************* //
-    // Flip the flag so that the JNI thread will update the buffer
-    isWritingFft = false;
+//     uint16_t outmag = 0;
+//     //set output buffer
+//     for(int i=0; i<FRAME_SIZE; i++){
+//         outmag = (uint16_t)sqrt(cx_out[i].r * cx_out[i].r + cx_out[i].i * cx_out[i].i);
+//         dataBuf->buf_[2*i] = (uint8_t)outmag;
+//         dataBuf->buf_[2*i+1] = (uint8_t)(outmag>>8);
+//     }
 
-    gettimeofday(&end, NULL);
-    LOGD("Time delay: %ld us",  ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
-}
+//     // thread-safe
+//     isWritingFft = true;
+//     // Currently set everything to 0 or 1 so the spectrogram will just be blue and red stripped
+//     /*for ( int i= 0; i < FRAME_SIZE; i++) {
+//         fftOut[i] = (i/20)%2;
+//     }*/
+
+//     /*
+//     Given a buffer size N and a sampling rate Fs, how much time do you have to complete your
+//     processing before the next buffer comes in?
+
+//     N * 1/Fs because we are getting Fs samples/sec, so each sample takes 1/Fs time to get.
+//     Since there are N samples per buffer, the buffer will fill up in N * 1/Fs seconds.
+//     */
+//     // ********************* END YOUR CODE HERE ************************* //
+//     // Flip the flag so that the JNI thread will update the buffer
+//     isWritingFft = false;
+
+//     gettimeofday(&end, NULL);
+//     LOGD("Time delay: %ld us",  ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+// }
 
 //number of frames we need to store to train filter
 #define FRAMES_STORED 20
